33966a938f28273e31cc6b3be19facb75d069851
EricGreenlee <eric.greenlee.96@gmail.com>
2023-02-22 15:09:13 -0500

    initial Arduino code commit

---
+++ WAVES_v0/WAVES_v0.ino
@@ -0,0 +1,20 @@
+/*
+  WAVES initial code
+
+  To do:
+  -pull data from distance sensor
+  -interface with SWARM modem
+  -calculate next pass
+  -sleep and save power
+
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
